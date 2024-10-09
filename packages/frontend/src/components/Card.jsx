0c4a0662fc2a6cad83dc95d2851435409b92e432
Alejo Kim Uy <alejokimuy@gmail.com>
2024-10-09 10:49:48 -0600

    Added masonry component

--- packages/frontend/src/components/Card.jsx
+++ packages/frontend/src/components/Card.jsx
@@ -3,7 +3,7 @@ import SvgPaintbrush from './svgs/SvgPaintbrush';
 import SvgTrashCan from './svgs/SvgTrashCan';
 import SvgPin from './svgs/SvgPin';
 
-function Card() {
+function Card({ cardTitle, cardBody }) {
 	function trimTitle(titleText) {
 		if (titleText.length > 25) {
 			let trimmedText = titleText.substring(0, 25);
@@ -15,32 +15,23 @@ function Card() {
 	}
 	return (
 		<>
-			<div className="flex flex-col items-center border-2 rounded-lg border-primary-border-color min-h-32 w-[172px] p-3">
+			<div className="flex flex-col items-center justify-evenly border-2 rounded-lg border-primary-border-color min-h-32 w-[172px] p-3">
 				{/* TITLE */}
 				<div className="flex justify-between w-full">
 					<div className="max-h-14 text-base max-w-28">
-						{trimTitle(`Lorem ipsum dolor sit amet consectetur, adipisicing
-						elit. Praesentium laboriosam explicabo voluptatem
-						deserunt molestias numquam labore ipsum adipisci, enim
-						suscipit consequatur similique veritatis dolore
-						perspiciatis modi porro, facere nulla architecto?`)}
+						{trimTitle(cardTitle)}
 					</div>
 					<div className="h-5 w-5">
 						<SvgPin></SvgPin>
 					</div>
 				</div>
 				{/* BODY */}
-				<div className="text-xs mt-3 mb-2">
-					Lorem ipsum odor amet, consectetuer adipiscing elit.
-					Elementum elit hendrerit cubilia viverra vulputate turpis.
-					Quis nascetur rhoncus turpis euismod ridiculus. Et laoreet
-					lacus potenti aliquam dapibus fermentum volutpat. Tristique
-					faucibus donec vitae tristique facilisi euismod elementum
-					vehicula taciti. Dignissim taciti finibus; efficitur
-					facilisis magnis porta diam. Eu eget viverra tempor
-					praesent; duis sed quam. Sapien vehicula laoreet mauris
-					imperdiet laoreet.
+				<div className="w-full">
+					<div className="text-xs mt-3 mb-2 text-left">
+						{cardBody}
+					</div>
 				</div>
+
 				{/* FOOTER */}
 				<div className="flex justify-evenly w-full h-5">
 					<SvgPaintbrush></SvgPaintbrush>
---
+++ packages/frontend/src/components/Deck.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import Card from './Card';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+
+function Deck() {
+	let cards = useState([
+		{
+			cardTitle: 'LONG',
+			cardBody:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. A numquam tempora doloribus modi laudantium iste repellendus, est eveniet excepturi, dignissimos cum enim ex aspernatur cupiditate ut magnam recusandae deserunt. Natus? Lorem ipsum dolor sit amet consectetur adipisicing elit. A numquam tempora doloribus modi laudantium iste repellendus, est eveniet excepturi, dignissimos cum enim ex aspernatur cupiditate ut magnam recusandae deserunt. Natus?',
+			color: 1,
+			isCheckbox: false,
+			isArchived: false,
+			isPinned: false,
+		},
+	]); 
+
+	return (
+		<>
+			<div className="ml-2 mt-2">
+				<Masonry columnsCount={2} gutter={5}>
+					<Card
+						cardTitle={'LONG'}
+						cardBody={
+							'Lorem ipsum dolor sit amet consectetur adipisicing elit. A numquam tempora doloribus modi laudantium iste repellendus, est eveniet excepturi, dignissimos cum enim ex aspernatur cupiditate ut magnam recusandae deserunt. Natus? Lorem ipsum dolor sit amet consectetur adipisicing elit. A numquam tempora doloribus modi laudantium iste repellendus, est eveniet excepturi, dignissimos cum enim ex aspernatur cupiditate ut magnam recusandae deserunt. Natus?'
+						}></Card>
+					<Card
+						cardTitle={'SHORT'}
+						cardBody={'This is a small card'}></Card>
+					<Card
+						cardTitle={'SHORT'}
+						cardBody={
+							'Lorem ipsum dolor sit amet consectetur adipisicing elit. A numquam tempora doloribus modi laudantium iste repellendus, est eveniet excepturi, dignissimos cum enim ex aspernatur cupiditate ut magnam recusandae deserunt. Natus?'
+						}></Card>
+					<Card
+						cardTitle={'LONG'}
+						cardBody={
+							'Lorem ipsum dolor sit amet consectetur adipisicing elit. A numquam tempora doloribus modi laudantium iste repellendus, est eveniet excepturi, dignissimos cum enim ex aspernatur cupiditate ut magnam recusandae deserunt. Natus?Lorem ipsum dolor sit amet consectetur adipisicing elit. A numquam tempora doloribus modi laudantium iste repellendus, est eveniet excepturi, dignissimos cum enim ex aspernatur cupiditate ut magnam recusandae deserunt. Natus?'
+						}></Card>
+				</Masonry>
+			</div>
+		</>
+	);
+}
+
+export default Deck;
